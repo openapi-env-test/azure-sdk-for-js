651824b7e3285c46e45acf34370efe66e0d88e69
Wei Dong <dw511214992@gmail.com>
2021-06-09 15:29:56 +0800

    test

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-inputCofig=$1
-specFolder=`jq .specFolder $inputCofig`
+set -x
+inputCofig=`cat $1`
+specFolder=`echo $inputCofig | jq .specFolder`
 specFolderTrim=`echo $specFolder | sed 's/\"//g'`
 npm install -g js-sdk-changelog-tool
 npm install -g gulp-cli
@@ -8,6 +9,7 @@ npm install -g gulp-cli
 for readmeFile in `echo $inputCofig | jq .relatedReadmeMdFiles | jq .[]`;
 do
   readmeFileTrim=`echo $readmeFile | sed 's/\"//g'`
+  echo $readmeFileTrim
   wholePath=$specFolderTrim/$readmeFileTrim
   gulp automation_generate --use @microsoft.azure/autorest.typescript@4.2.2 --readme $wholePath
 done
