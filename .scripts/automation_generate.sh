a04e01b22b855867c9b507752337993de0309176
Wei Dong <dw511214992@gmail.com>
2021-06-11 14:40:36 +0800

    test

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -3,6 +3,7 @@ set -x
 # install some libraries
 npm install -g @microsoft/rush
 rush update
+npm install
 
 # generate
 gulp automation_generate --inputJson $1 --outputJson $2 --use @microsoft.azure/autorest.typescript@4.2.2
