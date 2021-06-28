8f046d971432e02202b8a928863f1ebab9ecbe84
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2021-06-28 11:02:08 +0800

    Update automation_generate.sh

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-# install some libraries
+# rush update
 npm install -g @microsoft/rush
 rush update
-npm install
-
 # generate
 gulp automation_generate_in_pipeline --inputJson $1 --outputJson $2 --use @microsoft.azure/autorest.typescript@4.2.2
