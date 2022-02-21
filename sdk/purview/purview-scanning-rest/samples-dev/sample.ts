3d13e64b00325f09d2ee4db2acd254f9987cce67
SDKAuto <sdkautomation@microsoft.com>
2022-02-21 07:07:57 +0000

    CodeGen from PR 3027 in openapi-env-test/azure-rest-api-specs Merge 427317a3a4fc6617242d2aaed04d817b19047a24 into 48ca14276294a935695781416455a59424829d29

---
+++ sdk/purview/purview-scanning-rest/samples-dev/sample.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * This sample demonstrates how get a list of collections
+ * @summary gets a list of typedefs for entities
+ * @azsdk-weight 40
+ */
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function main() {
+  console.log("== Sample ==");
+}
+
+main().catch(console.error);
