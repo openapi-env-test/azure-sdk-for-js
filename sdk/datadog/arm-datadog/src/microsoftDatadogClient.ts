8df5a6714d5c14bdec4e4c82e76442065a2f4c6e
SDKAuto <sdkautomation@microsoft.com>
2022-03-31 08:23:19 +0000

    CodeGen from PR 3084 in openapi-env-test/azure-rest-api-specs Merge 4aca5f0635c7d6db0a7560977322060b0e4d9e9d into f7459dc7abbe2d446f24073f2190979fbe184928

--- sdk/datadog/arm-datadog/src/microsoftDatadogClient.ts
+++ sdk/datadog/arm-datadog/src/microsoftDatadogClient.ts
@@ -56,7 +56,7 @@ export class MicrosoftDatadogClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-datadog/3.0.0`;
+    const packageDetails = `azsdk-js-arm-datadog/3.0.2`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
