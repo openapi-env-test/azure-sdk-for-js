59e0ea16bc8507ec221e7d606911842c9835b254
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2022-06-13 09:51:48 +0800

    Update automation_generate.sh

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@@6.0.0-alpha.20.20220608.1
+code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@6.0.0-alpha.20.20220608.1
