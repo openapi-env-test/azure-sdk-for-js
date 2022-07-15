e3dbf814a13cdef92e676aa6f0a148ffd561de02
SDKAuto <sdkautomation@microsoft.com>
2022-07-15 09:43:50 +0000

    CodeGen from PR 3192 in openapi-env-test/azure-rest-api-specs Merge a9511fe172987a8da42c3cfb811a038a5ab085e3 into c023b6a34b9ada7cf98e2965feeef119082da1fb

--- sdk/resources-subscriptions/arm-resources-subscriptions/src/subscriptionClient.ts
+++ sdk/resources-subscriptions/arm-resources-subscriptions/src/subscriptionClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import { SubscriptionsImpl, TenantsImpl } from "./operations";
 import { Subscriptions, Tenants } from "./operationsInterfaces";
@@ -45,7 +50,7 @@ export class SubscriptionClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-resources-subscriptions/2.0.2`;
+    const packageDetails = `azsdk-js-arm-resources-subscriptions/2.1.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -93,6 +98,35 @@ export class SubscriptionClient extends coreClient.ServiceClient {
     this.apiVersion = options.apiVersion || "2021-01-01";
     this.subscriptions = new SubscriptionsImpl(this);
     this.tenants = new TenantsImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   /**
