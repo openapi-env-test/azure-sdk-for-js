61b5303b12031212ddb2bbff4d16c9176786baa8
SDKAuto <sdkautomation@microsoft.com>
2022-07-20 02:54:29 +0000

    CodeGen from PR 3398 in openapi-env-test/azure-rest-api-specs Merge 8f5574352115c8bd8811c308f569be9ca4270d64 into 1eeef05ce48884979c91a07f286a82897b441f02

--- sdk/changes/arm-changes/src/changesClient.ts
+++ sdk/changes/arm-changes/src/changesClient.ts
@@ -8,6 +8,11 @@
 
 import * as coreClient from "@azure/core-client";
 import * as coreRestPipeline from "@azure/core-rest-pipeline";
+import {
+  PipelineRequest,
+  PipelineResponse,
+  SendRequest
+} from "@azure/core-rest-pipeline";
 import * as coreAuth from "@azure/core-auth";
 import { ChangesImpl } from "./operations";
 import { Changes } from "./operationsInterfaces";
@@ -46,7 +51,7 @@ export class ChangesClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-changes/1.0.0`;
+    const packageDetails = `azsdk-js-arm-changes/1.0.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -95,6 +100,35 @@ export class ChangesClient extends coreClient.ServiceClient {
     this.$host = options.$host || "https://management.azure.com";
     this.apiVersion = options.apiVersion || "2022-05-01";
     this.changes = new ChangesImpl(this);
+    this.addCustomApiVersionPolicy(options.apiVersion);
+  }
+
+  /** A function that adds a policy that sets the api-version (or equivalent) to reflect the library version. */
+  private addCustomApiVersionPolicy(apiVersion?: string) {
+    if (!apiVersion) {
+      return;
+    }
+    const apiVersionPolicy = {
+      name: "CustomApiVersionPolicy",
+      async sendRequest(
+        request: PipelineRequest,
+        next: SendRequest
+      ): Promise<PipelineResponse> {
+        const param = request.url.split("?");
+        if (param.length > 1) {
+          const newParams = param[1].split("&").map((item) => {
+            if (item.indexOf("api-version") > -1) {
+              return item.replace(/(?<==).*$/, apiVersion);
+            } else {
+              return item;
+            }
+          });
+          request.url = param[0] + "?" + newParams.join("&");
+        }
+        return next(request);
+      }
+    };
+    this.pipeline.addPolicy(apiVersionPolicy);
   }
 
   changes: Changes;
