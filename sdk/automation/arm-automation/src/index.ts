9595f3aba3d34f46cd8a6df8599f7d4383c92251
SDKAuto <sdkautomation@microsoft.com>
2022-07-21 04:26:10 +0000

    CodeGen from PR 2532 in test-repo-billy/azure-rest-api-specs Merge c8b091bc43d0b78db8941bdcaef0ce916fd580c6 into 57fb05f72bca448de7bdace582df8816ba0422ee

--- sdk/automation/arm-automation/src/index.ts
+++ sdk/automation/arm-automation/src/index.ts
@@ -6,4 +6,7 @@
  * Changes may cause incorrect behavior and will be lost if the code is regenerated.
  */
 
-export * from "./hybridRunbookWorkerGroupOperations";
+/// <reference lib="esnext.asynciterable" />
+export * from "./models";
+export { AutomationClient } from "./automationClient";
+export * from "./operationsInterfaces";
