19b1cd95dacd9c527d15d1bc3d8e0b9cfd0c2a9d
SDKAuto <sdkautomation@microsoft.com>
2022-07-28 06:49:44 +0000

    CodeGen from PR 3549 in openapi-env-test/azure-rest-api-specs Merge 925edf514efdc3fbc41b491e35cc697ea1599289 into 2edb9141c68b06028101c06b5cffb94f3fcb49f8

--- sdk/purview/purview-administration-rest/src/account/purviewAccount.ts
+++ sdk/purview/purview-administration-rest/src/account/purviewAccount.ts
@@ -1,22 +1,41 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+
 import { getClient, ClientOptions } from "@azure-rest/core-client";
 import { TokenCredential } from "@azure/core-auth";
-import { PurviewAccountRestClient } from "./clientDefinitions";
+import { PurviewAccountClient } from "./clientDefinitions";
 
-export function PurviewAccountClient(
+export function createClient(
   endpoint: string,
   credentials: TokenCredential,
   options: ClientOptions = {}
-): PurviewAccountRestClient {
+): PurviewAccountClient {
   const baseUrl = options.baseUrl ?? `${endpoint}`;
   options.apiVersion = options.apiVersion ?? "2019-11-01-preview";
   options = {
     ...options,
     credentials: {
-      scopes: ["https://purview.azure.net/.default"],
-    },
+      scopes: ["https://purview.azure.net/.default"]
+    }
   };
 
-  return getClient(baseUrl, credentials, options) as PurviewAccountRestClient;
+  const userAgentInfo = `azsdk-js-purview-administration-rest/1.0.0-beta.1`;
+  const userAgentPrefix =
+    options.userAgentOptions && options.userAgentOptions.userAgentPrefix
+      ? `${options.userAgentOptions.userAgentPrefix} ${userAgentInfo}`
+      : `${userAgentInfo}`;
+  options = {
+    ...options,
+    userAgentOptions: {
+      userAgentPrefix
+    }
+  };
+
+  const client = getClient(
+    baseUrl,
+    credentials,
+    options
+  ) as PurviewAccountClient;
+
+  return client;
 }
