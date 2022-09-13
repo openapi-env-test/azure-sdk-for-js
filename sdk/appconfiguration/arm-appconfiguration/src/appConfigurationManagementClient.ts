d4ddbaae17bbdba4a25ee3d077633d66472111c0
azure-sdk <no-reply@azure-sdk.com>
2022-09-13 09:31:28 +0000

    [auto]create by sdk generation

--- sdk/appconfiguration/arm-appconfiguration/src/appConfigurationManagementClient.ts
+++ sdk/appconfiguration/arm-appconfiguration/src/appConfigurationManagementClient.ts
@@ -62,7 +62,7 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-arm-appconfiguration/3.0.1`;
+    const packageDetails = `azsdk-js-arm-appconfiguration/3.1.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
@@ -133,7 +133,7 @@ export class AppConfigurationManagementClient extends coreClient.ServiceClient {
         if (param.length > 1) {
           const newParams = param[1].split("&").map((item) => {
             if (item.indexOf("api-version") > -1) {
-              return item.replace(/(?<==).*$/, apiVersion);
+              return "api-version=" + apiVersion;
             } else {
               return item;
             }
