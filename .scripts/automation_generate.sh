d454ffd4f59590943048763a69735e6ba41fc4e2
Wei Dong <40835867+dw511214992@users.noreply.github.com>
2022-10-20 11:40:20 +0800

    Update automation_generate.sh

--- .scripts/automation_generate.sh
+++ .scripts/automation_generate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@6.0.0-alpha.20.20220608.1 --cadlEmitter=dw-cadl-typescript
+code-gen-pipeline --inputJsonPath=$1 --outputJsonPath=$2 --use=@autorest/typescript@6.0.0-alpha.20.20220608.1 --cadlEmitter=@azure-tools/cadl-typescript
