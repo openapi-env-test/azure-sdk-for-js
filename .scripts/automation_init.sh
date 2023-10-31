e0342dfdb37df0d43c4d97972479e11440aa0c30
Mary Gao <yanmeigao1210@gmail.com>
2023-10-31 22:43:11 +0800

    Update debug

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.bashrc
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install v18.15.0 
@@ -9,3 +8,4 @@ npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
 whereis code-gen-pipeline
 whereis node
+. ~/.bashrc
