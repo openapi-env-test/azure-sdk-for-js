f1a2d2e44f199ac701cce0515a30c31137782390
Mary Gao <yanmeigao1210@gmail.com>
2023-10-31 19:07:17 +0800

    Update the node version

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -3,6 +3,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install v18.15.0
+nvm use system
+npm uninstall -g a_module
 npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
 whereis code-gen-pipeline
