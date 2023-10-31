4ee6cf1bb8e8d159997ee1456a77ed6bfcf6b4f5
Mary Gao <yanmeigao1210@gmail.com>
2023-10-31 18:30:39 +0800

    Update the node version

--- .scripts/automation_init.sh
+++ .scripts/automation_init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source $NVM_DIR/nvm.sh
 nvm install v18.15.0
 npm install -g @microsoft/rush@5.92.0
 npm install -g @azure-tools/js-sdk-release-tools
